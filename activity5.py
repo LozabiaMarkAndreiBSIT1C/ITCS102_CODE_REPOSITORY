05c97ba2f636dcc1ce43b7296caee1cbe94a4ff0
LozabiaMarkAndreiBSIT1C <lozabiamarcus@gmail.com>
2026-08-25 14:24:10 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,24 @@
+#operator
+#arithmetic
+# + addition
+# - subtraction 
+# / division
+# * multiplication 
+# // floor division 
+# ** exponential 
+# % modulus
+
+a = -10
+b = 10
+
+sum = a + b
+sub = a - b
+div = a / b 
+multi = a * b
+
+print("The sum of",a, "and",b, "is",sum)
+print("The subtraction of",a, "and",b, "is",sub)
+print("The division of",a, "by",b, "is",div)
+print("The multiplication of",a, "and",b, "is",multi)
+
+ 
