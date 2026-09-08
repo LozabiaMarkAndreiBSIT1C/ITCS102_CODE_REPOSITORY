abaa2fd077986916eb94e669e28faebde7935e2b
LozabiaMarkAndreiBSIT1C <lozabiamarcus@gmail.com>
2026-09-08 12:01:22 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,28 @@
+#relational operator demo 
+x = 5
+y = 5
+text1 = 'sample'
+text2 = 'Sample'
+
+#1
+print ( x > y )
+
+print ( x < y )
+
+print ( x >= y )
+
+print ( x <= y )
+
+print ( x == y )
+
+print ( text1 == text2) 
+
+print ( text1 > text2 )
+
+print ( text1 < text2 )
+
+print ( text1 >= text2 )
+
+print ( text1 <= text2 )
+
+
