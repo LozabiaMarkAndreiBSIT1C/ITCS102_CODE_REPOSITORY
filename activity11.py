60dbf17c10d0357d26ae0b2bb721bc2f7e5389e7
LozabiaMarkAndreiBSIT1C <lozabiamarcus@gmail.com>
2026-09-08 12:34:17 +0800

    Add files via upload

---
+++ activity11.py
@@ -0,0 +1,11 @@
+username = 'mark'
+password = 'iloveyou3000'
+
+u = input('Input Username --->')
+p = input('Input Password --->')
+
+if username == u and p == password : 
+	print("ACCESS GRANTED")
+else :
+	print("ACCESS DENIED")
+
