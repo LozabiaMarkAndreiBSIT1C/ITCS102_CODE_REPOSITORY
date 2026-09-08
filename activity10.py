51050c10b021438af1525ea101af60b0b25d9dff
LozabiaMarkAndreiBSIT1C <lozabiamarcus@gmail.com>
2026-09-08 12:12:07 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,12 @@
+a = 10 
+b = 8 
+c = 13 
+
+print ( a > b or c < a )
+print ( a > b or c < a and c == b )
+print ( not( c < a and c == b ))
+print ( not(a > b or c < a ))
+
+
+#Order of operator 
+#not, and, or 
