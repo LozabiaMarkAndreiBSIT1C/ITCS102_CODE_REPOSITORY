21ab397cfe80a7e392cd466f480812153de265d8
LozabiaMarkAndreiBSIT1C <lozabiamarcus@gmail.com>
2026-09-19 19:10:51 +0800

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,39 @@
+philippines = "Corrupt country"
+print("hello world", philippines)
+
+integer_variable = 10
+boolean_variable = True
+float_variable = 3.14
+string_variable = "Hello, Philippines!"
+
+ehey = "My name is Mark Andrei \t \rI am from the Philippines"
+print(ehey)
+
+operator = 5 + 3 * 2 - 4 / 2
+arithmetic = (5 + 3) * (2 - 4) / 2
+
+a = 10
+b = 5
+
+sum_result = a + b
+difference_result = a - b
+product_result = a * b
+quotient_result = a / b
+
+print("The sum of", a, "and", b, "is", sum_result)
+print("The difference of", a, "and", b, "is", difference_result)
+print("The product of", a, "and", b, "is", product_result)
+print("The quotient of", a, "and", b, "is", quotient_result) 
+
+name = input("Enter your name: ")
+print("Hello,", name, "! Welcome to the Philippines.")
+x = eval (input("Enter your age: "))
+print("You are", x, "years old.")
+print(type(x))
+
+total = 0
+number = eval(input("Enter a number --->"))
+total += number
+number = eval(input("Enter a number --->"))
+total -= number
+print("The total is:", total)
