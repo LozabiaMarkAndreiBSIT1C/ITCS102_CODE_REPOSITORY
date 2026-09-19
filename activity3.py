ed75bf32ae7020e4ff3a9eb1a89a6ca0a0d810e4
LozabiaMarkAndreiBSIT1C <lozabiamarcus@gmail.com>
2026-09-19 18:16:38 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,30 @@
+#integer variable
+height = 163
+lrn = 109743140004
+cp number = 09387522382
+gcash PIN = 0938
+
+#float varible 
+pi = 3.24
+price = 19.99
+temperature = 67.7
+golden ratio = 1.6180
+
+#string variable
+surname = "mark"
+birthday = "august 21 2008"
+hobby = "Playing games"
+favorite color = "Yellow"
+
+#boolean variable
+isSingle = True
+isPresent = True
+isSmart = False
+isRich = False
+
+#PRINT
+print("Hi! My name is", surname)
+print("My birthday is", birthday)
+print("My height is", height, "in cm")
+print(lrn,"is my lrd used in school")
+print("Are you sure?", isRaining)
